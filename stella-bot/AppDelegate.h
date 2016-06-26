b1dbca92de3633185231f5572f8c74fb422c20ff
Mohamed Hayibor <mohamedhayibor@gmail.com>
2016-06-25 22:48:44 -0400

    Initial Commit

---
+++ stella-bot/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  stella-bot
+//
+//  Created by Mohamed Hayibor on 6/25/16.
+//  Copyright © 2016 Mohamed Hayibor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ stella-bot/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stella-bot
+//
+//  Created by Mohamed Hayibor on 6/25/16.
+//  Copyright © 2016 Mohamed Hayibor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
